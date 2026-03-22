f966af9f47f48481b4f678fd306d21c33d1e0405
hamid114 <38387837+hamid114@users.noreply.github.com>
2026-03-23 02:28:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,15 @@
-const CACHE = 'ghallah-v10';
-const ASSETS = ['/','index.html','/manifest.json'];
+const CACHE = 'ghallah-v11';
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(r => {
-      const rc = r.clone();
-      caches.open(CACHE).then(c => c.put(e.request, rc));
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
